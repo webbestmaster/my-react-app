2fcb69e92b838a071b1861e7de2ebb8321ab3ab9
Dmitry Turovtsov <web.best.master@gmail.com>
2017-01-05 14:34:41 +0300

    add lodash

--- app/www/container/Country.js
+++ app/www/container/Country.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
+const _ = require('lodash');
 
 const data = require('../data/data.json');
 
 export default class Country extends Component {
 
-    _getCountryData(alpha3) {
-        return data.filter(country => country.alpha3 === alpha3)[0];
-    }
-
     render() {
 
         let alpha3 = this.props.params.alpha3;
 
-        let countryData = this._getCountryData(alpha3);
+        let countryData = _.find(data, {alpha3});
 
         return <div>
 
