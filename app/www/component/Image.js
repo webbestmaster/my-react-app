2751aa548ab10b4dce90a89a126b5dca18b9f7ca
Dmitry Turovtsov <web.best.master@gmail.com>
2017-01-09 20:14:16 +0300

    app in progress...

--- app/www/component/Image.js
+++ app/www/component/Image.js
@@ -1,52 +1,59 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 
 let OpenSeadragon = require('openseadragon');
 
-// TODO: добавить на онЛив дестрой
-// https://openseadragon.github.io/docs/OpenSeadragon.Viewer.html - вот тут список методов
-// на хоуме есть пример как забайндить онЛив
-
 class Image extends Component {
-
-    componentWillReceiveProps(nextProps) {
-
-        console.log(nextProps);
-
-    }
 
     componentDidMount() {
 
+        const viewportMargins = 50;
+
         let props = this.props;
-        props.router.setRouteLeaveHook(props.route, this.routerWillLeave);
 
-        let wrapperNode = this.refs.wrapper;
-
-        OpenSeadragon({
-            element: wrapperNode,
-            // prefixUrl: '',
-            tileSources:   {
+        let imageViewer = new OpenSeadragon({
+            element: this.refs.wrapper,
+            tileSources: {
                 type: 'image',
-                url:  '/img/' + props.params.image,
-                buildPyramid: false
+                url: '/img/' + props.params.image,
+                buildPyramid: true
             },
             showNavigationControl: false,
             navigationControlAnchor: false,
             showZoomControl: false,
-            showHomeControl: false
+            showHomeControl: false,
+            maxZoomLevel: 4.0,
+            minZoomLevel: 0.4,
+            viewportMargins: {
+                top: viewportMargins,
+                left: viewportMargins,
+                right: viewportMargins,
+                bottom: viewportMargins
+            }
+        });
+
+        props.router.setRouteLeaveHook(props.route, () => {
+            imageViewer.destroy();
+            imageViewer = null;
         });
 
     }
 
     render() {
         let screen = this.props.screen;
-        return <div ref="wrapper" style={{width: screen.width + 'px', height: screen.height + 'px' }}></div>;
+        return <div ref="wrapper" style={{width: screen.width + 'px', height: screen.height + 'px'}}></div>;
     }
 
 }
 
 Image.propTypes = {
-    // resizeScreen: PropTypes.func.isRequired
+    screen: PropTypes.shape({
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired
+    }),
+    params: PropTypes.shape({
+        image: PropTypes.string.isRequired
+    })
 };
 
 export default connect(state => ({
