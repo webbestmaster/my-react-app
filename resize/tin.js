db265ea21d77c0d164643b61b31e5c6f77262caa
Dmitry Turovtsov <web.best.master@gmail.com>
2017-01-17 16:44:12 +0300

    graphic optimizing

--- resize/tin.js
+++ resize/tin.js
@@ -6,12 +6,13 @@ let countryData = require('./data.json');
 
 let keys = [
     // 'f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp'
-    'h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5', //- kidmathgenius@gmail.com
+    // 'h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5', //- kidmathgenius@gmail.com
     // 'eSu5nMg0TSDairQWQC_Bx0h41PxKgKEp', //- mikhail.anisimau.play@gmail.com
     // 'f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp', //- dmitry.turovtsov@gmail.com
     // '_JsmPE63lCa9UsS45vlKWMlhBhRntoK8', //- logikaismekalka@gmail.com
     // 'uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an', //- web.best.master@gmail.com
     // 'RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu'  // - ae.fan.game@gmail.com
+    'btwad9HNZNcWf2GUvy-ff3qeloCODlsr' // - dmitriy.turovtsov@viaden.com
 ];
 
 let i = 0;
