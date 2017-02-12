0dddc83950cf1a26708f7b05a7b334c71f5350e5
Dmitry Turovtsov <web.best.master@gmail.com>
2017-02-13 00:03:44 +0300

    update about

--- app/www/component/About.jsx
+++ app/www/component/About.jsx
@@ -11,8 +11,9 @@ export default class About extends Component {
                 <p className="country__description">Программа содержит более 150 разных валют.</p>
                 <p className="country__description">Воспользуйтесь поиском, чтобы быстрее найти интересующую вас валюту.</p>
                 <h2 className="country__header_2">Материалы</h2>
-                <p className="country__description">Описание и изображения валют - FINANCE.UA (http://banknotes.finance.ua)</p>
+                <p className="country__description">Описание и изображения валют - http://finance.ua (http://banknotes.finance.ua)</p>
                 <p className="country__description">Изображения флагов государств - https://github.com/lipis/flag-icon-css</p>
+                <p className="country__description">Изображения карт государств - https://www.amcharts.com</p>
                 <p className="country__description">Иконка и фоновое изображение - собраны из кусков изображений, найденных на просторах интернета.</p>
                 <h2 className="country__header_2">Контакты</h2>
                 <p className="country__description">Сайт разработчика: https://statlex.github.io</p>
